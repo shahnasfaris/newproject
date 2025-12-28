ed1f7a28fe30d8ac6691d3409d3e8dc799aa7125
shahnasfaris <shahnakunhu94@gmail.com>
2025-12-28 19:30:47 +0400

    Add password reset URLs to marketplace

--- organic_market/marketplace/urls.py
+++ organic_market/marketplace/urls.py
@@ -71,7 +71,11 @@ urlpatterns = [
 
 
 
-     path('farmer/query/respond/<int:query_id>/', views.respond_query, name='respond_query'),
+    path('farmer/query/respond/<int:query_id>/', views.respond_query, name='respond_query'),
+    path( 'password-reset/',auth_views.PasswordResetView.as_view(template_name='marketplace/password_reset.html'),name='password_reset'),
+    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='marketplace/password_reset_done.html'),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='marketplace/password_reset_confirm.html'),name='password_reset_confirm'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(template_name='marketplace/password_reset_complete.html'),name='password_reset_complete'),
 
 
 
